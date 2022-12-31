f47dd20254fddcb9f56178673ce8d7082bff0e35
dis-coder0904 <80644981+vatsal-agarwal-20@users.noreply.github.com>
2023-01-01 02:28:03 +0530

    strings

---
+++ 53.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+string countAndSay(int n, string s="1") {
+        
+        if(n==1) return s;
+        
+        // string s= countAndSay(n-1);
+        string res="";
+        
+        int counter=0;
+        for(int i=0;i<s.length();i++)
+        {
+            counter++;
+            if(i== s.length()-1 || s[i] != s[i+1])
+            {
+                res=res+to_string(counter)+s[i];
+                counter=0;
+            }
+        }
+        return countAndSay(n-1,res);
+    }
+int main()
+{
+
+return 0;
+}
